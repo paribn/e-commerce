fdb5e33df711df6bcbb16aac830c095cbc0c2df8
perinov <novruzovaperi2@gmail.com>
2023-10-12 02:28:11 +0400

    created navbar responsive part

---
+++ assets/js/home.js
@@ -0,0 +1,11 @@
+// navbar
+
+
+// const toggleButton = document.getElementsByClassName('toggle_button')[0]
+// const navbarArea = document.getElementsByClassName('nav__list')[0]
+
+// const peri = toggleButton.addEventListener('click', () => {
+//     navbarArea.classList.add('nav__list')
+//     console.log(peri);
+// })
+
