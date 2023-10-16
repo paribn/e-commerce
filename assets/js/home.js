71c3170ea6276ab49faaa2b95e27439fddb4d6df
perinov <novruzovaperi2@gmail.com>
2023-10-17 02:20:39 +0400

    mobile responsive ready

--- assets/js/home.js
+++ assets/js/home.js
@@ -7,15 +7,48 @@ toggleButton.addEventListener("click", () => {
   navbarList.classList.toggle("active");
 });
 
-
 $(document).ready(function () {
   $(".slick-carousel").slick({
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 1,
-    autoplay:true,
+    autoplay: true,
     // dots: true,
     arrows: true,
+    responsive: [
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1008,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 500,
+        settings: "unslick",
+      },
+    ],
   });
 
   $(".slick-carousel").on("swipe", function (event, slick, direction) {
